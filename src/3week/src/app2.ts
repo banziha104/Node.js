f3879cd8a50822ecccf0dd8a166c1657b25e20e4
iyeongjun <they6687@naver.com>
2017-11-11 14:56:50 +0900

    Add Emmet

---
+++ src/3week/src/app2.ts
@@ -0,0 +1,41 @@
+var num = 2;
+var num2: number = 2;
+
+let boolArray : boolean[] = [];
+boolArray.push(true);
+
+let ab : any = 30;
+class A{
+
+}
+enum Level {
+    B,
+    S,
+    G
+}
+var a = (a,b) => {return a+b};
+
+class Base{
+    public x : number;
+    private y : number;
+    protected z : number;
+}
+var b = new Base();
+class Childe extends Base{
+    constructor(){
+        super();
+        this.z;
+    }
+}
+
+class Person{
+    constructor(public name : string){
+
+    }
+}
+
+
+interface Name{ //Name이라는 형태의 자료형이 가지고있는 스트럭쳐를 구성
+    first : string, //속성
+    second : string
+}
