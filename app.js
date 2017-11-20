9003d09baea2425967a00b64caa8290c0d4e5d82
iyeongjun <they6687@naver.com>
2017-11-20 21:24:34 +0900

    Add 4Week

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let express = require('express'); //익스프레스 모듈 불러오기
 let mongoose = require('mongoose'); // 몽구스 불러오기
 let autoIncrement = require('mongoose-auto-increment');
 
+/* path */
+let path = require('path');
 
 /*DB 컨넥션 체크*/
 let db = mongoose.connection; // 디비 컨넥션
@@ -21,6 +23,9 @@ let admin = require('./routes/admin'); // admin.js 불러오기
 
 let app = express(); //익스프레스 객체 생성
 let port = 3000;     //포트 번호 설정
+
+app.set('views', path.join(__dirname,'views')); // 현위치에 views 라는 폴더로 설정
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{      //url 라우팅
    res.send('first app');
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,11 @@ let router = express.Router(); // 익스프레스의 라우터 모듈 추가
 
 router.get('/',(req,res)=>{res.send('admin app 123')});
 
-router.get('/products',(req,res)=>{res.send('admin product')});
+router.get('/products',(req,res)=>{
+    res.render('admin/products.ejs', { //템플릿을 지정함, 두 번째 파라미터 : 넘겨줄 key value 객체
+        message : "nodejs",
+        data : "camp"
+    });
+});
 
 module.exports = router; // 작성한 라우터를 모듈화
